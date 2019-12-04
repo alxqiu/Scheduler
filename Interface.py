d9dfbcf15c21a3f7ec5416cfb5198422636ea3e0
alxqiu <alxqiu@gmail.com>
2019-12-03 17:29:27 -0800

    new changes for interface, including tested POST and GET requests.

--- Interface.py
+++ Interface.py
@@ -8,56 +8,60 @@
     #research "requests"", use "posts" and json, and pickle
         #pickle converts arguments into string, and trns into json
         #1: requests 2: pickle, 3: understand json
+
+    #verify security headers in the server, thats more of a complex task
 import requests
 import json
+import urllib.parse
 
 class Executor():
-    def __init__(self, host, port, path = None, params = None):
-        self.server_info = (host, port, path, params)
-        assert ((self.server_info[0] is not None) and (self.server_info[1] is not None))
-        self.url_str = "http://" + self.server_info[0] + ":" + self.server_info[1] + "/"
-        if self.server_info[2] is not None:
-            self.url_str += self.server_info[2]
-        if self.server_info[3] is not None:
-            self.url_str += "?" + self.server_info[3]
+    #delete this __init__ when I've finished server side implementation
+    def __init__(self, url, path = None, params = None):
+        self.url_str = url
+           
+        #recode this section into taking host and port as params
 
-        #server stores info of jobs in centralized location, so should manage encoding
+        print(self.url_str)
 
     def submit(self, fn, job_name, memory, *args, priority = 1, retries = 0, run_now = False, **kwargs):
-        #just as fn denotes the callable in futures, the submit(job_name....) should schedule the callable 
-        ####
-            #post here
-            #need a means to dynamically create key:value pairs for variable arguments:        
-        #format into a json file and then put it in through post, we can technically have the source address just be tuple of 
-            #host and port info.  
+        #just as fn denotes the callable in futures, the submit(job_name....) should schedule the callable          
         data  = {'fn': fn, 'job_name': job_name, 'memory': memory, 'priority': priority, 
-                 'retries': retries, 'run_now': run_now, 'func_args': *args, 'func_kwargs': **kwargs}
-        json_data = json.dumps(data)
-        r = requests.post(self.url_str, json = json_data)
-        ####
-        #this portion would facilitate communication of job specifications: job_name, size, priority, retries, send_data, force
-               #need implicit arguments here *args and **kwargs
-                #send w http or json
-                #for priority & force: can put those with the same priorities in the same queue, on the order they were received
-                #then upon submitting another with force, that would trump all others with same priority
+                 'retries': retries, 'run_now': run_now, 'func_args': args, 'func_kwargs': kwargs}
+        ###
+            #problem: based on the response of the server, the names of the args and kwargs can be returned but not the values
+            #current syntax appears to be correct, that won't spawn runtime error
+        ###
+        r = requests.post(self.url_str, json = data)
+            #check if the response takes the actual one:
+            #define here what the server should return back to me
+            #define acceptable values here, perhaps write documentation for the clear inputs/returns from the server.
+            #perhaps assertions.    
+        assert r.json() is not None
+            #not sure what this would look like for a lot of other urls though...
+            #so not sure what to check for.  
 
-        #the future object should hold the info to know the status of the job, just need to get 
-       #NOT the job and the processing itself, rather its a supervisor
-        new_future = Future(job_name, self.server_info, *args, **kwargs)
+        new_future = Future(fn, self.url_str, memory, priority, retries, args, kwargs)
         #the object doesn't have the job specifications itself, function_name should be the path
+       
+        #return new_future
+        return r
 
-        return new_future
-
-    #def map():
-        #can include map() later not now.  
-        
     #next I want to include a function that will grab info from the server
-    def grab(self, job_name):
-        #params for the desired info to grab from the server
-        PARAMS = {'job name': job_name, 'running': None, 'retries left': None, 'func_args': None, 'func_kwargs': None} 
-        r = request.get(url = url_str, params = PARAMS)
-        data = r.json()
+    #   we want:
+        #memory, priority, retries, running, success, exception
 
+    def grab(self, fn, job_name):
+        #params for the desired info to grab from the server
+        payload = {'fn':fn, 'job name': job_name, 'memory': None,  'priority': None, 
+                   'retries': None, 'running': None, 'query': True, 'success': False, 'exception': None} 
+            #do not need to get information from the job specific args and kwargs
+        r = request.get(url = self.url_str, params = payload)
+            #information returned by server is defined by server side script.
+
+        assert r.json() is not None
+            #assert other elements here
+        return r.json()
+        
     def shutdown(self, wait = True):
         #grab server info and try to assess if job is done or not
         self.server_info
@@ -75,19 +79,33 @@ class Executor():
 #the future object is responsible for getting inquiries from the client about the server
 #and should keep all the info for the server and name, and facilitate communication between server and client
 class Future():
-    def __init__(self, function_name, server_info, func_args, func_kwargs):
+    def __init__(self, function_name, server_info, memory, priority, retries, *args, **kwargs):
        self.job_name = function_name
-    def cancel(self, force = False):
+       #self.server_info = server_info
+       #assert ((self.server_info[0] is not None) and (self.server_info[1] is not None))
+       #self.url_str = "http://" + self.server_info[0] + ":" + self.server_info[1] + "/"
+        #self.url_str = "https:/template.url/generic_page/" + urllib.parse.urlencode()
+       #if self.server_info[2] is not None:
+       #     self.url_str += '/'+ self.server_info[2]
+       #if self.server_info[3] is not None:
+            #self.url_str += "/" + self.server_info[3]
+       self.url_str = server_info
+    #should I include a "force" parameter?
+    def cancel(self):
 
         return False
+
     def cancelled(self):
-        return True
+        return ((self.running) == False)
 
     def running(self):
-        return True
+        r = requests.get(self.url_str)
+        #grabs status of whether the job object is running or not
+        return r.running
+
     def done(self):
         return True
-    def result(self):
+    def result(self, timeout = None):
         success = False
 
         if success:
