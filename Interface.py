afbf92b069312961fbc5787d46b24a0f67d9edde
alxqiu <alxqiu@gmail.com>
2020-02-07 16:24:43 -0800

    Finalized Changes

--- Interface.py
+++ Interface.py
@@ -29,7 +29,6 @@ class Executor():
         ##new method signature:
             #submit(fn, *args, job_config, **kwargs):
         #########
-        #ask: should run_now displace the original job running and put that as "untouched" or should it wait?
     def submit(self, fn, *args, job_config = {}, **kwargs):
         #fn: denotes a function object: pickle it into a string. Doesn't store function itself, but its a fully qualified
         #name for the function. 
@@ -40,23 +39,14 @@ class Executor():
         data  = {'fn': fn, 'func_args': args, 'func_kwargs': kwargs}
             #the submitted callable "fn" should be executed as fn(*args, **kwargs)
         default_config = {'memory': None, 'priority': 1, 'retries': 0, 'run_now': False}
-        #if input is none, we send default values
-        #check for elements present in job_config but not in default keys, then adding appropriate values to default
-
         if len(job_config.keys()) is not 0:
             assert set(job_config.keys()) < set(default_config.keys())
             #this allows the new, explicitly defined values to override defaults.      
             default_config.update(job_config)
-        #regardless of whether or not job_config is explicitly defined, this must occur for functionality
-        #of the server, so that the data can default to certain values. If job_config is explicitly defined, 
-        #defined values won't be overwritten. 
-
+        #If job_config is explicitly defined, defined values won't be overwritten. 
         data.update(default_config)
-
         r = requests.post(self.url_str + '/submit-request', json = data)  
-            #change so that r is only the job_id in a dict
-
-        #defs of acceptable returns
+            #only sends back {'job_id': <job_id>}
         assert r.status_code == 200
         job_id = r.json().get('job_id')
         assert job_id is not None
@@ -114,9 +104,9 @@ class Future():
         return info_dict
         
     def cancel(self):
-        #check to see if it can be canceled, won't even attempt to cancel if it is running
-        if running():
-            #returns false if it is currently running
+        #won't even attempt to cancel if it is running or already cancelled
+        if running() or cancelled():
+            #returns false if it is currently running or already cancelled
             return False
         else: 
             r = requests.post(self.url_str + '/cancel-job', json = {'job_id': self.job_id})
@@ -183,24 +173,23 @@ class Future():
                 break
             if (timer > timeout):
                 raise TimeoutError
-        #per project parameters, this will return None if no exception is there
+        #per project parameters, this will return default value of r['exception']
+            #which is None
         return r['exception']
-
 
     #this just runs a function directly after the completion of the job
     #can assume these functions are only going to reference the job info itself, 
         #as they take the future object as the only parameter
         #they can be as simple as printing the results of the job
     def add_done_callback(self, fn):
-        self.job_id = 0
         waiting = True
         while (waiting):
             sleep(1.0)
             r = requests.get(url = self.url_str + '/grab-job-info', json = {'job_id': self.job_id})
             if (r.json['cancelled'] or r.json['complete']):
-                waiting = False
-        fn()
-           
+                break
+        #says that the callable fn should run with "future" as it's only argument
+        fn(self)
 
     class TimeoutError(Exception):
         #raise when there is a timeout on the results method for futures.
--- ServerSideScript.py
+++ ServerSideScript.py
@@ -15,19 +15,6 @@ from inspect import signature
 
 ServerSideScript = Flask(__name__)
 
-#this should be the place where the job info is stored:
-    #'<job_id>': {'fn': <fn>, 'func_args': <*args>, 'func_kwargs': <**kwargs>, 
-    #   'job_id': <job_id>, 'memory': <memory>, 'priority': <priority>, 
-    #   'retries': <retries>, 'run_now': run_now}
-#change data storage to pickle format, have the function name pickled
-    #convert each job back into object, and figure out which package and modules it is in, and load it for me
-
-#pickled and non pickled:
-    #only 'fn', 'func_args', and 'func_kwargs' will be pickled
-#data_for_jobs = {'19838182.391829': {'fn': 'placeHolder', 'func_args': [], 
- #   'func_kwargs': {}, 'job_id': 19838182.391829, 'memory': 1024, 
-  #  'priority': 2, 'retries': 1, 'run_now': True, 'complete': False, 
-   # 'running': False, 'cancelled': False, 'exception': None, 'result': None}}
 data_for_jobs = {}
 lock = threading.Lock()
 
@@ -37,31 +24,21 @@ def default():
 
 #sends back unique id for a function in str form so its static
 def function_encoder(fn):
+    #####placeholder method for "encoding" job_id
     return str(datetime.datetime.now().timestamp())
 server_running = True
-
-##make private later
-#runnable_jobs = dict()
-
 def job_manager():
-    #will make this non-global after finishing debug
-    global runnable_jobs
     while (server_running):
             #when checking, protect d_f_j using lock
         if len(data_for_jobs) > 0:
-            #update the viable jobs list
-                #####will make runnable jobs private to this function after correctly debugged
             runnable_jobs = dict()       
-            ###protect data_for_jobs dict() only precisely when its accessed
+            ###protect data_for_jobs dict() only when modified, not just when accessed
             for job in data_for_jobs.items():
                 #jobs are moved to tuple form when parsed in forloop
                 job_properties = job[1]
                 if (job_properties['cancelled'] == job_properties['complete']):
                     runnable_jobs[job[0]] = job[1]
             if (len(runnable_jobs) > 0):
-                    #####problem: 2/6/2020:
-                        #need to assure that jobs that receive cancel order are affected on runnable_jobs
-                        #and can be removed or removed from consideration here
                 target_id = list(runnable_jobs.keys())[0]
                 highest_prior = runnable_jobs.get(list(runnable_jobs.keys())[0]).get('priority')
                 #now it can trump priority, and defers to oldest. 
@@ -75,6 +52,9 @@ def job_manager():
                         highest_prior = job_properties.get('priority')
                         target_id = job[0]
                 run_job(target_id)
+            #as the runnable_jobs loop only runs to determine a final target_id,
+            #and is remade at every time a job needs to be ran, other jobs that 
+            #need to be cancelled will be taken down if cancelled
         else:
             time.sleep(0.5)
     return None
@@ -94,14 +74,17 @@ def run_job(job_id):
                 data_for_jobs.get(job_id)['result'] = fn(*func_args, **func_kwargs)
                 data_for_jobs.get(job_id)['running'] = False
                 break
-             except:
+             except e:
                 #we are sending all exceptions, even generic ones back to the interface
                 #should have abstractified code here for that
-                data_for_jobs.get(job_id)['exceptions'] = 'Exception_One' 
+                #how to reference the exception from this branch?
+                data_for_jobs.get(job_id)['exceptions'] = e 
         data_for_jobs.get(job_id) ['complete'] = True
         data_for_jobs.get(job_id)['running'] = False
     return None
 
+#don't need to cancel jobs that are running, only ones that are not either running or cancelled
+#this method won't even be reached if the job is verified as not running and not cancelled
 @ServerSideScript.route('/cancel-job', methods = ['POST'])
 def cancel_job():
     global data_for_jobs
@@ -113,9 +96,7 @@ def cancel_job():
     lock.release()
     return None
 
-
-#@ServerSideScript.route('/handleRequest', methods = ['POST', 'GET'])
-#returns the job_id, all that's needed to make the future object  
+ 
 @ServerSideScript.route('/submit-request', methods = ['POST'])
 def submit_request():
     global data_for_jobs
@@ -128,7 +109,7 @@ def submit_request():
     request_data['exception'] = None
     request_data['result'] = None  
 
-    #as data_for_jobs is modified somewhere else at the same time, it needs to be locked
+    #lock activates precisely in this spot, and only takes a short amount of time
     lock.acquire()
     data_for_jobs[request_data['job_id']] = request_data
     #data_for_jobs.update({request_data['job_id']: request_data})
@@ -136,7 +117,7 @@ def submit_request():
     return {'job_id': request_data['job_id']}
 
 
-#send back data_for_jobs but only specifically for the id requested. 
+#for debugging: send back data_for_jobs but only specifically for the id requested. 
 @ServerSideScript.route('/grab-job-info', methods = ['GET'])
 def grab_job_info():
     lock.acquire()
@@ -148,12 +129,6 @@ def grab_job_info():
 @ServerSideScript.route('/get-data-for-jobs', methods = ['GET'])
 def get_data_for_jobs():
     return data_for_jobs
-
-#also for debugging
-#@ServerSideScript.route('/get-runnable-jobs', methods = ['GET'])
-#def get_runnable_jobs():
- #   return runnable_jobs
-
 
 ###two generic requests that don't do anything
 @ServerSideScript.route('/get-request', methods = ['GET'])
